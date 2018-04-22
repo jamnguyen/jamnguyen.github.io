b57f0508b0993a591dbbd519e8f12294c9dc2c5b
Jam Nguyen <jam@jamnguyen.com>
2018-04-22 15:20:19 +0700

    Finalize Parallax Sliding

--- parallax_sliding/script.js
+++ parallax_sliding/script.js
@@ -8,12 +8,17 @@ class Slider {
     this.diffX;
     this.isDragging;
 
+    this.body;
     this.slider;
+    this.indicator = [];
     this.slides = [];
     this.currentSlide;
     this.currentIndex = 0;
 
+    this.body = document.getElementById('body');
+    this.bodyLayer = document.getElementById('body-layer');
     this.slider = document.getElementById('slider');
+    let indicator = document.getElementById('indicator');
 
     Array.from(document.getElementsByClassName('card-wrapper')).forEach((element, index) => {
       element.addEventListener('touchstart', (evt) => this.handleTouchStart(evt));
@@ -27,14 +32,26 @@ class Slider {
       let slide = element;
       slide.backgroundElement = backgroundElement;
       slide.foregroundElement = foregroundElement;
+      slide.startColor = this.getRandomColor();
+      slide.endColor = this.getRandomColor();
       this.slides.push(slide);
+
+      // Indicator
+      let indicatorItem = document.createElement("div");
+      indicatorItem.classList.add('indicator-item');
+      if (index === 0) {
+        indicatorItem.classList.add('indicator-item-highlight');
+      }
+      indicator.appendChild(indicatorItem);
+      this.indicator.push(indicatorItem);
     });
+    this.currentSlide = this.slides[this.currentIndex];
+    this.updateColor(this.currentIndex);
   }
   
   handleTouchStart(evt) {
     this.isDragging = true;
     this.startX = evt.x;
-    this.currentSlide = this.slides[this.currentIndex];
   }
 
   handleTouchMove(evt) {
@@ -76,7 +93,8 @@ class Slider {
     this.currentSlide.foregroundElement.style.transform = "translate(-50%, -50%)";
 
     this.slider.style.cssText = `transform: translateX(${-slideIndex*this.slides[0].offsetWidth}px)`;
-    this.currentIndex = slideIndex;
+    this.updateColor(slideIndex);
+    this.updateIndicator(slideIndex);
   }
 
   cancelSlide() {
@@ -85,6 +103,34 @@ class Slider {
     
     this.currentSlide.foregroundElement.style.transition = "transform ease-out 0.3s";
     this.currentSlide.foregroundElement.style.transform = "translate(-50%, -50%)";
+  }
+
+  updateIndicator(index) {
+    this.indicator[this.currentIndex].classList.remove("indicator-item-highlight");
+    this.indicator[index].classList.add("indicator-item-highlight");
+    this.currentIndex = index;
+    this.currentSlide = this.slides[this.currentIndex];
+  }
+
+  updateColor(nextIndex) {
+    let nextSlide = this.slides[nextIndex];
+
+    this.body.style.backgroundImage = "linear-gradient(" + this.currentSlide.startColor + ", " + this.currentSlide.endColor + ")";
+    this.bodyLayer.style.backgroundImage =  "linear-gradient(" + nextSlide.startColor + ", " + nextSlide.endColor + ")";
+    // this.bodyLayer.style.animation = "fadeOut 2s";
+    this.bodyLayer.classList.remove("fade-in");
+    void this.bodyLayer.offsetWidth;
+    this.bodyLayer.classList.add("fade-in");
+    console.log(this.body.style);
+  }
+
+  getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
   }
 }
 
